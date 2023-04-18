efef4f03a9dc37d5379b1f892ef137a06d6689ca
[John Wilkins] <[interruptingvolcano@protonmail.com]>
2023-04-17 22:31:03 -0500

    Add pointer-events to remove duplicate input error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const game = (function() {
 
   // Cache DOM
-  const grid = document.querySelector('.grid');
+
   const topLeft = document.querySelector('.top-left');
   const midTop = document.querySelector('.mid-top');
   const topRight = document.querySelector('.top-right');
@@ -12,11 +12,11 @@ const game = (function() {
   const midBottom = document.querySelector('.mid-bottom'
   );
   const bottomRight = document.querySelector('.bottom-right');
+  const boxInput = document.querySelectorAll('span');
   const playerPromptContainer = document.querySelector('.player-prompt-container')
   const playerXPrompt = document.querySelector('.playerX-prompt');
   const playerOPrompt = document.querySelector('.playerO-prompt');
   const instructions = document.querySelector('.instructions');
-  let value = 0;
 
   let gameBoard = [
       topLeft,
@@ -84,10 +84,15 @@ const game = (function() {
         oWins.push(1);
       };
   };
+
+  function addTicTac(target) {
+    
+  }
   
   // binder
 
-  gameBoard.forEach((target) => {
+  gameBoard.forEach((target) =>
+   {
     target.addEventListener('click', () => {
       if (xWins.length > 0 || oWins.length > 0 ||           playerPromptContainer.innerText === 'Game Over, it\'s a tie!!'
       ) {
@@ -95,54 +100,63 @@ const game = (function() {
       }
       let boxTarget = target;
       if (playChoice.length === 0 || playChoice.length % 2 === 0) {
-        let boxInput = document.createElement('span');
-        boxTarget.append(boxInput);
+        let boxInput = document.createElement('span');    
+        boxTarget.append(boxInput);    
         boxInput.innerHTML = 'X';
         boxInput.style.color = 'green';
         playChoice.push('X');
         target = 'X';
         if (boxTarget.className === 'top-left') {
           X_left.push(target) && X_top.push(target) &&
-          X_tLeftDiagonal.push(target);
+          X_tLeftDiagonal.push(target); 
+          boxTarget.classList.add('pointer'); 
         };
         if (boxTarget.className === 'mid-top') {
           X_vMiddle.push(target);
           X_top.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'top-right') {
           X_right.push(target);
           X_top.push(target);
           X_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-left') {
           X_left.push(target);
           X_hMiddle.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'center') {
           X_vMiddle.push(target);
           X_hMiddle.push(target);
           X_tLeftDiagonal.push(target);
           X_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-right') {
           X_right.push(target);
           X_hMiddle.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'bottom-left') {
           X_left.push(target);
           X_bottom.push(target);
           X_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-bottom') {
           X_vMiddle.push(target);
           X_bottom.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'bottom-right') {
           X_right.push(target);
           X_bottom.push(target);
           X_tLeftDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
-
+        
         checkGame();
 
         // check for tie
@@ -166,52 +180,60 @@ const game = (function() {
         if (boxTarget.className === 'top-left') {
           O_left.push(target) && O_top.push(target) &&
           O_tLeftDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-top') {
           O_vMiddle.push(target);
           O_top.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'top-right') {
           O_right.push(target);
           O_top.push(target);
           O_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-left') {
           O_left.push(target);
           O_hMiddle.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'center') {
           O_vMiddle.push(target);
           O_hMiddle.push(target);
           O_tLeftDiagonal.push(target);
           O_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-right') {
           O_right.push(target);
           O_hMiddle.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'bottom-left') {
           O_left.push(target);
           O_bottom.push(target);
           O_tRightDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'mid-bottom') {
           O_vMiddle.push(target);
           O_bottom.push(target);
+          boxTarget.classList.add('pointer');
         };
         if (boxTarget.className === 'bottom-right') {
           O_right.push(target);
           O_bottom.push(target);
           O_tLeftDiagonal.push(target);
+          boxTarget.classList.add('pointer');
         };
 
         checkGame();
 
         playerOPrompt.classList.add('hide');
         playerXPrompt.classList.remove('hide');
-
-      }    
-    })
+      }        
+    })  
   });
 
   return { Players }
